59b73b7d5014dc4580019cbcf9bc9e962f966313
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 13:52:25 -0400

    Add `Masfile` placeholder

--- init.zsh
+++ init.zsh
@@ -56,8 +56,9 @@ header "Installing/updating Homebrew packages"
 brew bundle --file=./config/homebrew/Brewfile
 
 if [[ $(uname -s) = "Darwin" ]]; then
-    header "Installing Mac applications from Caskfile"
+    header "Installing Mac applications"
   brew bundle --file=./config/homebrew/Caskfile
+  brew bundle --file=./config/homebrew/Masfile
 fi
 
 # Symlink dotfiles
