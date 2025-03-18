8025ffe95441bedde3b6c7c8776ac68fce44d5c2
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 15:35:46 -0400

    Add `altermo/ultimate-autopair.nvim`

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -111,4 +111,14 @@ return {
       "marilari88/neotest-vitest",
     },
   },
+
+  -- pairs
+  {
+    'altermo/ultimate-autopair.nvim',
+    event={'InsertEnter','CmdlineEnter'},
+    branch='v0.6', --recommended as each new version will have breaking changes
+    opts={
+      --Config goes here
+    },
+  },
 }
