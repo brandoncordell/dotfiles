01a2fddf9126de070c44fda6d3befaa40487e9ce
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 01:23:42 -0400

    Start fleshing out `init.zsh`

---
+++ init.zsh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+source ./zsh/functions/utils/pretty_print.zsh
+
+# Some helper functions for printing messages
+# without polluting the file
+function header() {
+  print
+  pretty_print "$1" "blue"
+  print
+}
+
+function warn() {
+  pretty_print "$1" "yellow"
+}
+
+function error() {
+  pretty_print "$1" "red"
+}
+
+function success() {
+  pretty_print "\u2714 $1" "green"
+}
+
+function info() {
+  pretty_print "$1" "white"
+}
+
+header "Setting up dev environment"
+
+if ! [[ -v XDG_CONFIG_HOME ]]; then
+  warn '$XDG_CONFIG_HOME not set. Setting to $HOME/.config' yellow
+  export XDG_CONFIG_HOME=$HOME/.config
+fi
+
+# Ensure $HOME/Code exists
+if ! [[ -d $HOME/Code ]]; then
+  warn '$HOME/Code directory not found. Creating it' yellow
+  mkdir $HOME/Code
+else
+  success '$HOME/Code directory already exists'
+fi
+
