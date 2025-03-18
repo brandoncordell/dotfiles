556423f0d15502cd0434964c530d9cb2b939c493
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 15:34:58 -0400

    Add `glepnir/dashboard-nvim'

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -56,6 +56,16 @@ return {
   { 'neovim/nvim-lspconfig' },
   { 'hrsh7th/nvim-cmp' },
   { 'hrsh7th/cmp-nvim-lsp' },
+
+  -- dashboard
+  {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup()
+    end,
+    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  },
   {
     'folke/which-key.nvim',
     event = 'VeryLazy',
