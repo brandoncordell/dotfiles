5e462d4bbeee618f517c46c2e1d6de78d16ae29b
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 15:10:34 -0400

    Add `$STRICT` for work-only machines
     Prompt whether this is a work or personal machine and only install personal apps if it's not a work machine.

--- init.zsh
+++ init.zsh
@@ -1,10 +1,27 @@
 #!/usr/bin/env zsh
 
+STRICT=false
+
 source ./install/utils.zsh
 
+header "Installing your dotfiles"
 
+# Is this a work machine?
+while [[ ! "$use_strict" =~ ^[yYnN]$ ]]; do read "use_strict?$(pp "Is this a work machine? (y/n): ")"; done
 
-header "Setting up dev environment"
+case $use_strict in
+  y|Y) STRICT=true;;
+  n|N) STRICT=false;;
+esac
+
+if $STRICT; then
+  export HOMEBREW_STRICT=true
+
+  echo ''
+  warn "Strict mode enabled" "yellow"
+fi
+
+header "Setting up environment"
 
 if ! [[ -v XDG_CONFIG_HOME ]]; then
   warn '$XDG_CONFIG_HOME not set. Setting to $HOME/.config' yellow
