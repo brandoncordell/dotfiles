6e836674e28d482582805b0302b5274dea17c524
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 01:24:15 -0400

    Install Homebrew, if not already installed

--- init.zsh
+++ init.zsh
@@ -41,3 +41,11 @@ else
   success '$HOME/Code directory already exists'
 fi
 
+header "Installing system dependencies"
+
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  success "Homebrew already installed"
+fi
