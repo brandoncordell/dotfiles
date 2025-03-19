095e2aeddda2a404e8be25032901e4d7196d9023
Brandon Cordell <brandon@brandoncordell.com>
2025-03-18 20:41:26 -0400

    wezterm: Hide tab bar if there's only one tab

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,4 +7,6 @@ config.color_scheme = 'nord'
 config.font = wezterm.font("FiraCode Nerd Font", {weight=450, stretch="Normal", style="Normal"})
 config.font_size = 14
 
+config.hide_tab_bar_if_only_one_tab = true
+
 return config
