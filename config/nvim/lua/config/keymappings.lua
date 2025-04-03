68d4cbf4850d047ec6701d8403d93bdc200b7473
Brandon Cordell <brandon@brandoncordell.com>
2025-04-03 15:16:00 -0400

    neovim: Add diagnostic plugins
    * Add`folke/trouble.nvim`
    * Add `rachartier/tiny-inline-diagnostic.nvim`

--- config/nvim/lua/config/keymappings.lua
+++ config/nvim/lua/config/keymappings.lua
@@ -1,3 +1,6 @@
 vim.keymap.set('n', ';', ':') -- Less shift for productivity
 vim.keymap.set('n', '<Leader><Leader>', '<C-^>') -- Quickly switch between the last two files
 vim.keymap.set('n', '<Leader>hl', ':set nohlsearch<CR>') -- Clear search highlights
+
+vim.keymap.set('n', '<C-[>', '<cmd>lua vim.diagnostic.jump({ count = -1 })<CR>')
+vim.keymap.set('n', '<C-]>', '<cmd>lua vim.diagnostic.jump({ count = 1 })<CR>')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -20,6 +20,71 @@ return {
     end
   },
 
+  -- diagnostics
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "LspAttach",
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup({
+        preset = 'modern'
+      })
+
+      vim.diagnostic.config({
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '●',
+            [vim.diagnostic.severity.WARN] = '●',
+            [vim.diagnostic.severity.HINT] = '●',
+            [vim.diagnostic.severity.INFO] = '●',
+          }
+        },
+        underline = false,
+        update_in_insert = false,
+        virtual_text = false
+      })
+    end
+  },
+
   -- snippets
   { "rafamadriz/friendly-snippets" },
   {
