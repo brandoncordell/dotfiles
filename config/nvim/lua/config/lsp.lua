d2561981c3a35a6539580be7905fbce4f3d7fba8
Brandon Cordell <brandon@brandoncordell.com>
2025-04-12 22:29:15 -0400

    neovim: Tweak LSP config

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -3,31 +3,76 @@ local lspconfig = require("lspconfig")
 
 lsp_zero.extend_lspconfig()
 
-lsp_zero.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(_, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'bashls',
-    'cssls',
-    'eslint',
-    'html',
-    'lua_ls',
-    'rubocop',
-    'solargraph',
-    'tailwindcss',
-    'ts_ls',
-    'volar'
-  },
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"bashls",
+		"cssls",
+		"eslint",
+		"html",
+		"lua_ls",
+		"rubocop",
+		"solargraph",
+		"tailwindcss",
+		"ts_ls",
+		"volar",
+	},
+	handlers = {
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+	},
 })
 
+-- cssls
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.cssls.setup({})
+
+-- emmet
 lspconfig.emmet_language_server.setup({})
+
+-- lua_ls
+lspconfig.lua_ls.setup({
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+				path ~= vim.fn.stdpath("config")
+				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+			then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				version = "LuaJIT",
+			},
+
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+			},
+		})
+	end,
+	settings = {
+		Lua = {},
+	},
+})
+
+-- rubocop
+lspconfig.rubocop.setup({})
+
+-- tailwindcss
+require("lspconfig").tailwindcss.setup({})
